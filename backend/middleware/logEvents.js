4ed8b4b59894e4a027ae3e35d46555fef8845a1c
Patrick Martin <patrickwaltermartin@gmail.com>
2024-07-17 11:40:12 -0300

    Add sending logs to a text file

--- backend/middleware/logEvents.js
+++ backend/middleware/logEvents.js
@@ -1,13 +1,27 @@
+import { existsSync, mkdirSync, appendFileSync } from 'fs'
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url'
+
 const getDate = function(){
     let d = new Date();
-    return `${d.getFullYear()}-${d.getDate()}-${d.getMonth()+1}T${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+    return `${d.getFullYear()}-${d.getDate()}-${d.getMonth()+1}T${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
 
 }
-//Logging
 
 const reqLogger = (req, _, next) => {
-    console.log(`${getDate()}\t${req.method}\t${req.headers.origin}\t${req.url}`)
+    const logConent = `${getDate()}\t${req.method}\t${req.headers.origin}\t${req.url}`;
+    console.log(logConent);
+    writeLogsToFile('reqLogs.txt', logConent);
     next()
+};
+
+const writeLogsToFile = (logFile, logContent) => {
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    const logsPath = join(__dirname, '..', 'logs'); 
+    if(!existsSync(logsPath)){
+        mkdirSync(logsPath);
+    }
+    appendFileSync(join(logsPath,logFile), logContent+'\n');
 };
 
 export {reqLogger}
